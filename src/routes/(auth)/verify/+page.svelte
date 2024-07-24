d75d14ba5c05829e94daec5dae0f2c90b7043249
Adam Shand <adam@shand.net>
2024-07-24 15:05:38 +1200

    working verify route

---
+++ src/routes/(auth)/verify/+page.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+	import { browser, dev } from '$app/environment';
+	import { goto, invalidateAll } from '$app/navigation';
+	import { onDestroy, onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { pb } from '$lib/pocketbase.svelte';
+
+	import Debug from '$lib/components/Debug.svelte';
+
+	let isLoading = $state(false);
+	const pbUser = $state($page.data.user);
+	const pbUserId = $derived($page.data.user?.id);
+
+	onMount(async () => {
+		!pbUser && goto('/');
+		pbUser?.verified && goto('/app');
+
+		pb.authStore.loadFromCookie(document.cookie);
+		pb.collection('users').subscribe(pbUserId, async (e) => {
+			dev && console.log(`${e.action}: verified=${e.record.verified}`);
+			invalidateAll();
+
+			// if (e.record.verified) {
+			// 	await pb.collection('users').authRefresh();
+			// 	goto('/app');
+			// }
+		});
+	});
+	onDestroy(() => {
+		pb.collection('users').unsubscribe(pbUserId);
+	});
+
+	async function handleForm() {
+		isLoading = true;
+
+		try {
+			if (pbUser) {
+				await pb.collection('users').requestVerification(pbUser.email);
+			}
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (err: any) {
+			dev && console.error('(dev) verify/+page.svelte: ', err.message);
+		} finally {
+			isLoading = false;
+		}
+	}
+</script>
+
+<article>
+	<h3>Verify your account</h3>
+
+	<p>
+		Hey <mark>{pbUser?.name?.split(' ')[0]}</mark>, welcome to Play. One last step and your account
+		will be active.
+	</p>
+	<p>
+		We have sent a verification email to <mark>{pbUser?.email}</mark>
+	</p>
+	<p>Please find the email, and click on the link included to automatically verify your account.</p>
+
+	<p>If you can't find the verification email, please check your spam folder.</p>
+
+	<section>
+		<button aria-busy={isLoading} onclick={() => handleForm()} type="submit">
+			Resend Verification Email
+		</button>
+	</section>
+
+	<Debug>
+		<ul>
+			<li>username: {pbUser?.username}</li>
+			<li>verified: {pbUser?.verified}</li>
+		</ul>
+	</Debug>
+</article>
+
+<style>
+	h3 {
+		text-align: center;
+	}
+	section {
+		display: flex;
+		justify-content: center;
+	}
+	p,
+	button {
+		margin-bottom: 1rem;
+	}
+	mark {
+		color: var(--brand);
+		background: none;
+		padding-inline: 0;
+	}
+</style>
