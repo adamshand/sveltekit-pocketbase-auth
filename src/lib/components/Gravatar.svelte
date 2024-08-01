81e4ee1ab88087a79bed12b6db7bb692c0b28d52
Adam Shand <adam@shand.net>
2024-08-01 13:30:16 +1200

    added fallback image

--- src/lib/components/Gravatar.svelte
+++ src/lib/components/Gravatar.svelte
@@ -1,24 +1,29 @@
 <script lang="ts">
 	import type { GravatarUrl } from '$lib/types'
-	import { getGravatarUrl } from '$lib'
+	import { getGravatarUrl, getOpenmojiUrl } from '$lib'
 
 	let { border = false, email, option = '404', round = false, size = '192' }: GravatarUrl = $props()
 
-	const gravatarUrl = getGravatarUrl(email) + `?d=${option}&s=${size}`
+	let gravatarUrl = $state(getGravatarUrl(email) + `?d=${option}&s=${size}`)
+
+	function handleError() {
+		gravatarUrl = getOpenmojiUrl('274C')
+	}
 </script>
 
 <img
+	alt="gravatar"
 	class:border
 	class:round
-	style={`width: ${size}px; height: ${size}px;`}
-	alt="gravatar"
+	onerror={handleError}
 	src={gravatarUrl}
+	style={`width: ${size}px; height: ${size}px;`}
 />
 
 <style>
 	.border {
 		border: 1px solid var(--brand);
-		padding: 3px;
+		padding: 0.5rem;
 	}
 	.round {
 		border-radius: 50%;
--- src/routes/app/gravatar/+page.svelte
+++ src/routes/app/gravatar/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/stores'
 	import Gravatar from '$lib/components/Gravatar.svelte'
+
+	const email = $page.data.user?.email ?? 'dummy@example.nz'
 </script>
 
 <h1>Gravatars</h1>
+
 <div>
-	<Gravatar email={$page.data.user.email} size="64" />
-	<Gravatar border email={$page.data.user.email} size="128" />
-	<Gravatar email={$page.data.user.email} round size="256" />
+	<Gravatar {email} size="64" option="wavatar" />
+	<Gravatar {email} size="128" option="mp" />
+	<Gravatar border {email} round size="256" />
 	<Gravatar border email={'blahblah@blah.com'} round size="256" option="robohash" />
 	<Gravatar email={'blahblah@blah.com'} size="256" option="retro" />
 </div>
