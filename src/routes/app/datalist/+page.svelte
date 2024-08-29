8996eeebbe88b1c7ddd7fda49ad2ccea42822644
Adam Shand <adam@shand.net>
2024-08-29 17:30:54 +1200

    example datalist

---
+++ src/routes/app/datalist/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	const things = ['foo', 'bar', 'baz']
+</script>
+
+<input list="things" />
+<datalist id="things">
+	{#each things as thing}
+		<option value={thing}></option>
+	{/each}
+</datalist>
