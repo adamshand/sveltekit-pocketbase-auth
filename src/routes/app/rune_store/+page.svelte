db6e2e16d9f5199df49b4f994c42c0609ffac560
RoffeDH <roffedh@gmail.com>
2024-10-15 22:08:32 +0200

    Connected to database

--- src/routes/app/rune_store/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Decrement from './decrement.svelte'
-	import { store } from './store.svelte'
-</script>
-
-{store.count}
-<button onclick={() => store.count++}> + </button>
-
-<Decrement />
